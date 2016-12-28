94c6810496779a629af641d3ea69ff4b67ddb9fd
iconoclastpython <ico.yigang@hotmail.com>
2016-12-28 00:02:11 -0500

    house rober

--- normal/dp.java
+++ normal/dp.java
@@ -22,3 +22,45 @@ public class Solution {
         return Math.max(preNo, preYes);
     }
 }
+-------------------------------------------------
+
+House Robber II:
+public class Solution {
+    private int robLine(int[] nums, int low, int high) {
+        int preNo = 0, preYes = 0;
+        for(int i = low; i <= high; i++) {
+            int tmp = preNo;
+            preNo = Math.max(preNo, preYes);
+            preYes = nums[i] + tmp;
+        }
+        return Math.max(preNo, preYes);
+    }
+    
+    public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        return Math.max(robLine(nums, 0, nums.length - 2), robLine(nums, 1, nums.length - 1));
+    }
+}
+-------------------------------------------------
+
+House Robber III:
+public class Solution {
+    public int rob(TreeNode root) {
+        int[] res = robSub(root);
+        return Math.max(res[0], res[1]);
+    }
+    
+    private int[] robSub(TreeNode root) {
+        if(root == null) return new int[2];
+        
+        int[] left = robSub(root.left);
+        int[] right = robSub(root.right);
+        int[] res = new int[2];
+        
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] + right[0];
+        
+        return res;
+    }
+}
+-------------------------------------------------
